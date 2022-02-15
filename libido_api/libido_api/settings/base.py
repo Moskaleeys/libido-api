36a52e231ac5efcff67901ffe7aae712f0a1d39a
leopard627 <elastic7327@gmail.com>
2022-02-16 00:27:08 +0900

    fix deposit code

--- libido_api/libido_api/settings/base.py
+++ libido_api/libido_api/settings/base.py
@@ -241,8 +241,6 @@ STATIC_URL = "/static/"
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_HOST_USER = "test.moska.bc@gmail.com"
 EMAIL_HOST_PASSWORD = "##ahtmzk999"
 EMAIL_PORT = 587
