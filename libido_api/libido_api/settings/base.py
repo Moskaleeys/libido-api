f22f7bb9aec1985571db91f29c88240fc91f0c75
leopard627 <elastic7327@gmail.com>
2022-02-18 20:48:27 +0900

    fx

--- libido_api/libido_api/settings/base.py
+++ libido_api/libido_api/settings/base.py
@@ -244,7 +244,7 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = "test.moska.bc@gmail.com"
 EMAIL_HOST_PASSWORD = "##ahtmzk999"
 EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
